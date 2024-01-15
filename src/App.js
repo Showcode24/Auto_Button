94c83788c7dda3761ed2030bfb0fa95a6d4681dc
Showcode24 <emmanuelsoniran005@gmail.com>
2024-01-15 11:27:29 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Toogle from './Toogle';
+
+function App() {
+  return (
+    <div className="App">
+   <Toogle/>
+    </div>
+  );
+}
+
+export default App;
