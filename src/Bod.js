6639d6910fa21555d78259640be6e40ef4b5bfcd
Showcode24 <emmanuelsoniran005@gmail.com>
2024-01-30 12:08:29 -0800

    Add conditional error and loading message

--- src/Bod.js
+++ src/Bod.js
@@ -2,39 +2,42 @@ import { useEffect, useState } from "react";
 import ContentList from "./contentList";
 const Bod = () => {
     const [contents, setContents] = useState(null);
-      /*  [
-            { id: 1, author: "Tobi", title: "Italian Marble Wood", body: "Lore Ipsum...." },
-            { id: 2, author: "Emmanuel", title: "Dawnish Walnut", body: "Lore Ipsum...." },
-            { id: 3, author: "Ayooluwa", title: "African Fibre", body: "Lore Ipsum...." }
-        ]
-    );
-    */
-
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
     const [course, setCourse] = useState('frontend')
 
     useEffect(() => {
-        fetch('http://localhost:7000/contents')
-            .then((response) => {
-             return response.json()
-            })
-            .then((contentsArray) => {
-                console.log(contentsArray)
-                setContents(contentsArray)
-            })
+        setTimeout(() => {
+            fetch('http://localhost:7000/contentsz')
+                .then((response) => {
+                    if (!response.ok) {
+                        throw Error('Service is currently busy. Please try again later')
+                    }
+                    return response.json()
+                })
+                .then((contentsArray) => {
+                    setContents(contentsArray)
+                    setLoading(false)
+                })
+                .catch((err) => {
+                    console.log(err.message)
+                    setError(err.message)
+                    setLoading(false)
+                })
+        }, 3000)
     }, []);
 
 
-    const handleDelete = (id) => {
-        console.log('The id of the clickedd blog is', id)
-
-        const filteredcontents = contents.filter((contents, index) => contents.id !== id)
-        setContents(filteredcontents)
-    }
     return (
         <div className="Bod">
             <h1>The Furniture <br></br>you would love</h1>
-            {contents && <ContentList contents={contents} handleDelete={handleDelete} />}
-            <button onClick={() => setCourse('backend')}>Click to Change Course</button>
+            {loading && <div>Loading...</div>}
+
+            {error && <div>{error}</div>}
+            
+            {contents && <ContentList contents={contents} />}
+            
+            {/* <button onClick={() => setCourse('backend')}>Click to Change Course</button> */}
 
             <p>{course}</p>
         </div>
--- src/contentList.js
+++ src/contentList.js
@@ -1,11 +1,11 @@
-const ContentList = ({contents, handleDelete}) => {
+const ContentList = ({contents}) => {
     return ( 
         <>
         {contents.map((contents, index) => (
             <div className="board" key={contents.id}>
                 <h2>Title: {contents.title}</h2>
                 <p>Written by {contents.author}</p>
-                <button onClick={()=>{handleDelete(contents.id)}} className="button">Delete Blog</button>
+                
             </div>
         ))}
         </>
