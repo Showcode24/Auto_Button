70be8e9e4a2e9aacf10edd50f7367d23c82ddcff
Showcode24 <emmanuelsoniran005@gmail.com>
2024-02-04 04:16:48 -0800

    Adding of Custom Hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import './App.css';
-import Toogle from './Toogle';
+// import Toogle from './Toogle';
 import Navbar from './Navbar';
 import Bod from './Bod';
 import Meth from './Meth';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import About from './component/about';
+import Contact from './component/contact';
+import Create from './component/create';
+
 function App() {
   return (
     <div className="App">
-       <Navbar/>
-      <Bod/>
-   <Toogle/>
-  <Meth/>
-      </div>
+
+      <Navbar />
+      <Bod />
+      
+      
+        <Routes>
+          <Route path='/' element={<Bod />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      
+
+
+      {/* <Toogle /> */}
+      {/* <Meth /> */}
+    </div>
   );
 }
 
