d2d5ca1630b2a9ea9dbb1a24971e3ebff8fb9d05
Showcode24 <emmanuelsoniran005@gmail.com>
2024-02-04 04:13:56 -0800

    Adding Custom Hooks

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,21 @@
 import { IoCartOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return ( 
-        <div className="navbar">
+        <nav className="navbar">
+
             <p>NOVA</p>
+            <div className="links">
             <ul>
+                <Link to='/'>Home</Link>
                 <li>Home</li>
                 <li>About</li>
                 <li>Furniture</li>
                 <li>Contact</li>
                 <IoCartOutline />
             </ul>
-        </div>
+            </div>
+        </nav>
      );
 }
  
