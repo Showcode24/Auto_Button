9d332f58f9732c0af78a2c7545bb6cd15d4bcfc1
Showcode24 <emmanuelsoniran005@gmail.com>
2024-02-05 13:07:43 -0800

    Add Custom Hooks

--- src/Bod.js
+++ src/Bod.js
@@ -1,32 +1,12 @@
 import { useEffect, useState } from "react";
 import ContentList from "./contentList";
+import useFetch from "./hooks/use-fetch";
+
 const Bod = () => {
-    const [contents, setContents] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
+    
     const [course, setCourse] = useState('frontend')
 
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:7000/contents')
-                .then((response) => {
-                    if (!response.ok) {
-                        throw Error('Service is currently busy. Please try again later')
-                    }
-                    return response.json()
-                })
-                .then((contentsArray) => {
-                    setContents(contentsArray)
-                    setLoading(false)
-                })
-                .catch((err) => {
-                    console.log(err.message)
-                    setError(err.message)
-                    setLoading(false)
-                })
-        }, 3000)
-    }, []); //empty deendency array => run once
-
+  const {contents, error, loading} = useFetch('http://localhost:7000/contents');
 
     return (
         <div className="Bod">
@@ -34,9 +14,9 @@ const Bod = () => {
             {loading && <div>Loading...</div>}
 
             {error && <div>{error}</div>}
-            
+
             {contents && <ContentList contents={contents} />}
-            
+
             {/* <button onClick={() => setCourse('backend')}>Click to Change Course</button> */}
 
             <p>{course}</p>
