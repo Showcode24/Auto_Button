151ccae24d3dde5e5538eb0dfeb2ed393f5d0d55
Showcode24 <emmanuelsoniran005@gmail.com>
2024-02-07 06:59:20 -0800

    Add Details and Forms

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import About from './component/about';
 import Contact from './component/contact';
 import Create from './component/create';
 import NotFound from './component/not-found';
+import ContentDetails from './component/content-details';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/create' element={<Create />} />
           <Route path='/contact' element={<Contact />} />
+          <Route path='/contents/:number'element={<ContentDetails />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       
